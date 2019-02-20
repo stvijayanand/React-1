69aadac93ee2fbc382486a2f2f65c02b4880c4b7
Vijayanand Settin <stvijayanand@hotmail.com>
2019-02-20 12:48:39 -0800

    First checkin

--- src/App.js
+++ src/App.js
@@ -3,13 +3,44 @@ import "./App.css";
 import Person from "./Person/Person";
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: "vijay", age: 37 },
+      { name: "arvind", age: 28 },
+      { name: "chris", age: 56 }
+    ],
+    someOtherState: "some text"
+  };
+
+  switchNameHandler = () => {
+    this.setState({
+      persons: [
+        { name: "vijayanand", age: 37 },
+        { name: "arvind", age: 28 },
+        { name: "chris", age: 57 }
+      ]
+    });
+  };
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I am a React App.</h1>
-        <Person name="vijay" age="37" />
-        <Person name="arvind" age="28" />
-        <Person name="chris" age="56" />
+        <button onClick={this.switchNameHandler}>Switch name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+        >
+          My Hobbies: Racing
+        </Person>
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        />
       </div>
     );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,8 +1,13 @@
 import React from "react";
 
-const person = (props) => {
+const person = props => {
   return (
-    <p>I am {props.name} and I am {props.age} years old!</p>
+    <div>
+      <p>
+        I am {props.name} and I am {props.age} years old!
+      </p>
+      <p>{props.children}</p>
+    </div>
   );
 };
 
